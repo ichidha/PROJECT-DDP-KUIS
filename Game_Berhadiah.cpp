44feeb8e4ed0fffca557b8cf9bb09ee3f3711a46
NUR RAMADHANI <kasdhan43@gmail.com>
2024-10-18 22:24:20 +0700

    Update Game_Berhadiah.cpp

--- Game_Berhadiah.cpp
+++ Game_Berhadiah.cpp
@@ -29,57 +29,57 @@ int main() {
     cout << "OWO: Halo tuan " << nama << ", terimakasih telah hadir untuk mengikuti game kali ini.\n";
     cout << "OWO: Untuk mempersingkat waktu mari kita MULAI......!!!\n";
 
-    cout << "OWO: Oke " << nama <<", di antara tangan saya dan sebuah box mana yang akan kamu pilih?\n";
+    cout << "OWO: Oke " << nama <<", di antara (tangan) saya dan sebuah (box) mana yang akan kamu pilih?\n";
     string pilihan1;
     cout << nama << ": "; cin >> pilihan1;
     while (pilihan1 != "tangan" && pilihan1 != "box") {
-        cout << "OWO: sekali lagi tangan saya atau sebuah box mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi (tangan) saya atau sebuah (box) mana yang akan kamu pilih?\n";
         cout << nama << ": "; cin >> pilihan1;
     }
-    cout << "OWO : Apakah kamu yakin? ";
+    cout << "OWO : Apakah kamu yakin? (ya/tidak) ";
     string opsi1;
     cin >> opsi1;
     
     while (opsi1 == "tidak") {
-        cout << "OWO: sekali lagi tangan saya atau sebuah box mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi (tangan) saya atau sebuah (box) mana yang akan kamu pilih?\n";
         cout << nama << ": "; cin >> pilihan1;
-        cout << "OWO : Apakah kamu yakin? ";
+        cout << "OWO : Apakah kamu yakin? (ya/tidak)";
         cin >> opsi1;
     }
     cout << endl;
-    cout << "OWO: Selanjutnya di antara " << pilihan1 << " dan kertas di kantong saya mana yang akan kamu pilih?\n";
+    cout << "OWO: Selanjutnya di antara (" << pilihan1 << ") dan kertas di (kantong) saya mana yang akan kamu pilih?\n";
     string pilihan2;
     cout << nama << ": "; cin >> pilihan2;
     while (pilihan2 != "kantong" && pilihan2 != pilihan1) {
-        cout << "OWO: sekali lagi sesuatu di kantong saya atau " << pilihan1 << " mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi sesuatu di (kantong) saya atau (" << pilihan1 << ") mana yang akan kamu pilih?\n";
         cout << nama << ": "; cin >> pilihan2;
     }
-    cout << "OWO : Apakah kamu yakin? ";
+    cout << "OWO : Apakah kamu yakin? (ya/tidak) ";
     string opsi2;
     cin >> opsi2;
     
     while (opsi2 == "tidak") {
-        cout << "OWO: sekali lagi sesuatu di kantong saya atau " << pilihan1 << " mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi sesuatu di (kantong) saya atau (" << pilihan1 << ") mana yang akan kamu pilih?\n";
         cout << nama << ": "; cin >> pilihan2;
-        cout << "Apakah kamu yakin? ";
+        cout << "Apakah kamu yakin? (ya/tidak)";
         cin >> opsi2;
     }
     cout << endl;
-    cout << "OWO: Terakhir di antara " << pilihan2 << " dan sebuah amplop mana yang akan kamu pilih?\n";
+    cout << "OWO: Terakhir di antara (" << pilihan2 << ") dan sebuah (amplop) mana yang akan kamu pilih?\n";
     string pilihan3;
     cout << nama << ": "; cin >> pilihan3;
     while (pilihan3 != "amplop" && pilihan3 != pilihan2) {
-        cout << "OWO: sekali lagi " << pilihan2 << " dan sebuah amplop mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi (" << pilihan2 << ") dan sebuah (amplop) mana yang akan kamu pilih?\n";
         cin >> pilihan3;
     }
-    cout << "OWO : Apakah kamu yakin? ";
+    cout << "OWO : Apakah kamu yakin? (ya/tidak)";
     string opsi3;
     cin >> opsi3;
     
     while (opsi3 == "tidak") {
-        cout << "OWO: sekali lagi amplop atau " << pilihan2 << " mana yang akan kamu pilih?\n";
+        cout << "OWO: sekali lagi (amplop) atau (" << pilihan2 << ") mana yang akan kamu pilih?\n";
         cout << nama << ": "; cin >> pilihan3;
-        cout << "Apakah kamu yakin? ";
+        cout << "Apakah kamu yakin? (ya/tidak)";
         cin >> opsi3;
     }
     cout << endl;
